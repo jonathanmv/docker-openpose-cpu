bcfd3353e2b09b266efdc0a123cc1f7c58ce9db3
jonathanmv <jonathan.morales@jimdo.com>
2020-03-24 22:38:17 +0100

    fix: handle sqs event instead of s3 event

--- lambda/src/index.test.ts
+++ lambda/src/index.test.ts
@@ -1,5 +1,5 @@
 import {handler} from "./index";
-import {Context, S3CreateEvent} from "aws-lambda";
+import {Context, S3CreateEvent, SQSEvent} from "aws-lambda";
 import AWS from 'aws-sdk';
 
 import {CONVERT_TASK, PROCESS_TASK} from "./ecsTaskRequestBuilder";
@@ -54,6 +54,27 @@ const buildS3CreateEvent = (key: string): S3CreateEvent => ({
     ]
 });
 
+const buildSQSEvent = (s3ObjectKey: string): SQSEvent => ({
+  Records: [
+    {
+      messageId: "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+      receiptHandle: "MessageReceiptHandle",
+      body: JSON.stringify(buildS3CreateEvent(s3ObjectKey)),
+      attributes: {
+        ApproximateReceiveCount: "1",
+        SentTimestamp: "1523232000000",
+        SenderId: "123456789012",
+        ApproximateFirstReceiveTimestamp: "1523232000001"
+      },
+      messageAttributes: {},
+      md5OfBody: "7b270e59b47ff90a553787216d55d91d",
+      eventSource: "aws:sqs",
+      eventSourceARN: "arn:aws:sqs:eu-west-2:123456789012:MyQueue",
+      awsRegion: "eu-west-2"
+    }
+  ]
+});
+
 beforeEach(() => {
     mockRunTask.mockClear();
 });
@@ -71,7 +92,7 @@ test.each([
     ['conversion/people.avi', {taskDefinition: PROCESS_TASK, destination: 'processing/people.avi'}],
     ['processing/people.avi', {taskDefinition: CONVERT_TASK, destination: 'processed/people.mp4'}],
 ])('handles "%s" with "%o"', async (key: string, expectedParams: ExpectedRunTaskParams) => {
-    const event = buildS3CreateEvent(key);
+    const event = buildSQSEvent(key);
     const context = {} as Context;
     const callback = () => {
     };
@@ -95,7 +116,7 @@ test.each([
     ['conversion/people.mp4', 1],
     ['processing/people.mp4', 1],
 ])('', async (key: string, numberOfCalls: number) => {
-    const event = buildS3CreateEvent(key);
+    const event = buildSQSEvent(key);
     const context = {} as Context;
     const callback = () => {
     };
--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,18 +1,28 @@
-import { S3CreateEvent, S3Handler } from "aws-lambda";
+import { SQSEvent, SQSHandler, S3CreateEvent } from "aws-lambda";
 import {
   handle
 } from './s3CreateEventHandler'
 
-export const handler: S3Handler = async (event: S3CreateEvent) => {
-  const { bucket, object } = event.Records[0].s3;
-  const { key } = object;
-  const source = `s3://${bucket.name}/${key}`;
-  console.log("Handling " + source);
+const getS3Event = (sqsEvent: SQSEvent): S3CreateEvent => {
   try {
+    return JSON.parse(sqsEvent.Records[0].body);
+  } catch (error) {
+    console.error("Couldn't object s3 event from sqs event", sqsEvent);
+    throw error;
+  }
+}
+
+export const handler: SQSHandler = async (event: SQSEvent) => {
+  try {
+    const s3event = getS3Event(event);
+    const { bucket, object } = s3event.Records[0].s3;
+    const { key } = object;
+    const source = `s3://${bucket.name}/${key}`;
+    console.log("Handling " + source);
     await handle(source);
     console.log("Done handling " + source);
   } catch (error) {
-    console.error("Failed handling " + source);
+    console.error("Failed handling event", event);
     throw error;
   }
 };
