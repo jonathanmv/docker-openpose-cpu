7eb380d83a721f10841729cef83a55187cb63769
jonathanmv <jonathan.morales@jimdo.com>
2020-03-26 07:47:49 +0100

    fix: do not force conversion to avi. use file ext

--- docker-images/download-convert-upload.sh
+++ docker-images/download-convert-upload.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+set -e
 
 # Converts a file in s3 to .avi and uploads it to the specified bucket
 # Expects 2 positional params:
 # $1 file to be downloaded from s3
 # $2 file s3 destination
 
-# example: 
+# example:
 # $ ./download-convert-upload.sh s3://original-bucket/convert.mp4 s3://destination-bucket/convert.avi
 # $ file uploaded to s3://destination-bucket/convert.avi
 
 # No params can be passed to ffmpeg for now
 
 rnd=$RANDOM
-originalFile="/tmp/original-${rnd}"
-convertedFile="/tmp/converted-${rnd}.avi"
+basePath="/tmp/convert-${rnd}"
+mkdir -p $basePath
+
+originalFileName=$(basename ${1})
+convertedFileName=$(basename ${2})
+originalFilePath="${basePath}/$originalFileName"
+convertedFilePath="${basePath}/$convertedFileName"
 
 echo "Downloading $1"
-aws s3 cp $1 $originalFile
+aws s3 cp $1 $originalFilePath
 
-echo "Converting $1 to .avi"
-ffmpeg -i $originalFile $convertedFile
-echo "Converted file $convertedFile"
+echo "Converting $1"
+ffmpeg -i $originalFilePath $convertedFilePath
+echo "Converted file $convertedFilePath"
 
 echo "Uploading converted file to $2"
-aws s3 cp $convertedFile $2
+aws s3 cp $convertedFilePath $2
 
-rm -f $originalFile
-rm -f $convertedFile
+rm -f $originalFilePath
+rm -f $convertedFilePath
 
 echo "File uploaded to $2"
